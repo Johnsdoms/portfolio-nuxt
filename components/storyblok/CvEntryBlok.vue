677ce0130176d5b4b7a78735586ad7d6a4fd17c0
Jonas Hamann <jonashamann@icloud.com>
2025-01-13 16:05:35 +0100

    add dark mode handling

--- components/storyblok/CvEntryBlok.vue
+++ components/storyblok/CvEntryBlok.vue
@@ -32,13 +32,18 @@
                     v-html="textContent"
                 />
 
-                <NuxtImg
-                    v-if="blok.company_logo"
-                    class="cv-entry-blok__logo"
-                    width="140px"
-                    provider="storyblok"
-                    :src="blok.company_logo.filename"
-                />
+                <div
+                    class="cv-entry-blok__logo-wrapper"
+                    :class="{ 'cv-entry-blok__logo-wrapper--has-background': isDark }"
+                >
+                    <NuxtImg
+                        v-if="blok.company_logo"
+                        class="cv-entry-blok__logo"
+                        width="140px"
+                        provider="storyblok"
+                        :src="blok.company_logo.filename"
+                    />
+                </div>
             </div>
         </div>
     </div>
@@ -46,9 +51,12 @@
 
 <script setup lang="ts">
 import dayjs from "dayjs";
+import { usePreferredDark } from "@vueuse/core";
 
 const props = defineProps({ blok: Object });
 const textContent = computed(() => renderRichText(props.blok ? props.blok.description : ""));
+
+const isDark = usePreferredDark();
 
 function formatDate(date: string) {
     return dayjs(date).format("MMMM YYYY");
@@ -90,6 +98,24 @@ function formatDate(date: string) {
         max-height: 140px;
         align-self: flex-start;
         object-fit: contain;
+
+        &-wrapper {
+            &--has-background {
+                display: flex;
+                justify-content: center;
+                background-color: var(--c-white);
+                padding: 20px;
+                width: 100px;
+                height: 100px;
+                border-radius: 100%;
+
+                img {
+                    max-width: 100px;
+                    max-height: 80px;
+                    align-self: center;
+                }
+            }
+        }
     }
 }
 </style>
