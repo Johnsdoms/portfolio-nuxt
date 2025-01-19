9fcbe20f84bb4951b4c15d7664425a13f47da747
Jonas Hamann <jonashamann@icloud.com>
2025-01-19 12:30:54 +0100

    add theme changing favicon for supported browsers

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { useFavicon, usePreferredDark } from "@vueuse/core";
+
 useHead({
     title: "// jonashamann.com",
     meta: [
-        { name: "description", content: "Welcome to my portfolio site." },
+        { name: "description", content: "This is the website of: Jonas." },
     ],
 });
+
+const isDark = usePreferredDark();
+const favicon = computed(() => isDark.value ? "favicon_dark.svg" : "favicon.svg");
+
+useFavicon(favicon);
 
 const story = await useAsyncStoryblok("home", { version: import.meta.env.VITE_STORYBLOK_MODE });
 </script>
